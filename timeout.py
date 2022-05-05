5d7a845e086901fc5af0b45798b2da4b1fba9fe3
Jordan Bottoms <jbttoms@gmail.com>
2022-05-05 02:33:23 -0500

    Update timeout.py
    Corrected a comment. Originally was going to use index as the variable name

--- timeout.py
+++ timeout.py
@@ -3,7 +3,7 @@ timewaster = 1000000 # lower or increase to whatever works for you.
 def timeout():
     global loop
     global timewaster
-    # Previously preset 1000000. Now just change the index variable
+    # Previously preset 1000000. Now just change the timewaster variable
     print("Will count to " + timewaster + " to time out session")
     for counter in range(timewaster):
         print("Timing session out: ",counter)
